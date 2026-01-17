f45349000aa623bc2e4d51f2e87fdec3c7e04d0c
Vinay Panchal <vinayadsmi4@gmail.com>
2026-01-17 20:30:20 +0530

    feat(inputfield): feature

--- ui-library/src/index.ts
+++ ui-library/src/index.ts
@@ -1,6 +1,6 @@
 // Main entry point for the ui-library
 // This file exports all components so they can be imported by other applications
-// Usage in consuming app: import { Button, Icon, Input, Dropdown, RadioButton, Checkbox } from 'ui-library'
+// Usage in consuming app: import { Button, Icon, Input, Dropdown, RadioButton, Checkbox, TextField } from 'ui-library'
 
 export { Button } from './components/Button/Button';
 export { Icon } from './components/Icon/Icon';
@@ -8,3 +8,5 @@ export { Input } from './components/Input/Input';
 export { Dropdown } from './components/Dropdown/Dropdown';
 export { RadioButton } from './components/RadioButton/RadioButton';
 export { Checkbox } from './components/Checkbox/Checkbox';
+export { TextField } from './components/TextField/TextField';
+
